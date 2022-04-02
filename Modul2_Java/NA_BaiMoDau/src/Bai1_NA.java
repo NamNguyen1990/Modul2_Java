227239142f325beb52eb096222c08b9d5e46100d
NAM <namxuan.nguyen1990@gmail.com>
2022-04-02 20:47:02 +0700

    Bai1_NA_LamquenJava

---
+++ Modul2_Java/NA_BaiMoDau/src/Bai1_NA.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Bai1_NA {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập số a");
+        a = scanner.nextInt();
+        System.out.println("Nhập số b");
+        b = scanner.nextInt();
+        System.out.println("Tổng 2 số là:" + " " + (a + b));
+    }
+}
