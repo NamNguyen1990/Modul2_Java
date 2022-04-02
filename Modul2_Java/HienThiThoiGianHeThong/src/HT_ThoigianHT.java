33fd91f8974f48ee764a486fca4b428f58d53ae4
NAM <namxuan.nguyen1990@gmail.com>
2022-04-02 21:28:30 +0700

    HT_TimeHT

---
+++ Modul2_Java/HienThiThoiGianHeThong/src/HT_ThoigianHT.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class HT_ThoigianHT {
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is:"+ now);
+    }
+}
